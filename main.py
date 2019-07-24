f4911522f6a40276afca7de452c44577f84fb574
Aston <cssi@cssis-MacBook-Pro.local>
2019-07-24 09:48:45 -0700

    [import ndb]

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import ndb
 
 class ModelWithUser(ndb.Model):
     user_id = ndb.StringProperty()
     color = ndb.StringProperty()
-
 
     def get_by_user(cls, user):
         return cls.query().filter(cls.user_id == user.user_id()).get()
