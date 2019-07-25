7c38bac582d7c26c971ea89382b3184743a326cf
Aston <cssi@cssis-MacBook-Pro.local>
2019-07-24 18:30:10 -0700

    add Like class/kind (Alex plan) and brainstorm with Michael

--- seed_function.py
+++ seed_function.py
@@ -10,3 +10,10 @@ class Item(ndb.Model):
     img_url = ndb.StringProperty(required = True)
     description = ndb.StringProperty(required = True)
     owner = ndb.KeyProperty(User)
+
+class Like(ndb.Model):
+    item = ndb.KeyPropery(Item)
+    owner = ndb.KeyProperty(User)
+
+    liker_id = ndb.IntegerProperty(required = True)
+    item_id = ndb.IntegerProperty(required = True)
