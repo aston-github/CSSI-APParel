9bf62fede2c59fdd69a76d45451404db10e024ad
Aston <cssi@cssis-MacBook-Pro.local>
2019-07-25 15:16:27 -0700

    basically the rest of the program was written with Xoshil; matching algorithm, Like kind for DataStore, match page, feed page, ID recording and etc

--- seed.py
+++ seed.py
@@ -1,10 +1,14 @@
-from seed_function import User, Item
+from seed_function import UserProfile, Item, Like
 
 def seed_data():
-    nancy_key = User(user_id = 001, first_name = 'Nancy', last_name = 'Sarabia', email = 'nanccysarabiaa12@gmail.com').put()
-    xoshil_key = User(user_id = 002, first_name = 'Xoshil', last_name = 'Chen-Marquez', email = 'xoshilcm@gmail.com').put()
+    nancy = UserProfile(user_id = "nancy", first_name = 'Nancy', last_name = 'Sarabia', email = 'nanccysarabiaa12@gmail.com').put()
+    xoshil = UserProfile(user_id = "xoshil", first_name = 'Xoshil', last_name = 'Chen-Marquez', email = 'xoshilcm@gmail.com').put()
+    aston = UserProfile(user_id = "aston", first_name = 'Aston', last_name = 'Yong', email = 'astontyong@gmail.com').put()
 
-    nancy_item1 = Item(img_url = 'https://bigboze.com/wp-content/uploads/2017/08/Vintage-90s-Bootleg-Selena-T-Shirt.jpg', description = 'Vintage Selena T-Shirt, Size: Small', owner = nancy_key)
-    xoshil_item1 = Item(img_url = 'https://auctions.c.yimg.jp/images.auctions.yahoo.co.jp/image/dr000/auc0407/users/20489ec3f1aed236c91063314c14ec48936aab30/i-img1200x842-1530700458efjyqp6130.jpg', description = 'Converse All Star Black Plaid, Size: US-7', owner = xoshil_key)
+    nancy_item1 = Item(img_url = 'https://bigboze.com/wp-content/uploads/2017/08/Vintage-90s-Bootleg-Selena-T-Shirt.jpg', description = 'Vintage Selena T-Shirt, Size: Small', owner = nancy)
+    xoshil_item1 = Item(img_url = 'https://auctions.c.yimg.jp/images.auctions.yahoo.co.jp/image/dr000/auc0407/users/20489ec3f1aed236c91063314c14ec48936aab30/i-img1200x842-1530700458efjyqp6130.jpg', description = 'Converse All Star Black Plaid, Size: US-7', owner = xoshil)
     nancy_item1.put()
     xoshil_item1.put()
+
+    xoshil_like1 = Like(liker = xoshil, item = nancy_item1.key, owner = nancy).put()
+    aston_like1 = Like(liker = aston, item = nancy_item1.key, owner = nancy).put()
--- seed_function.py
+++ seed_function.py
@@ -1,7 +1,7 @@
 from google.appengine.ext import ndb
 
-class User(ndb.Model):
-    user_id = ndb.IntegerProperty(required = True)
+class UserProfile(ndb.Model):
+    user_id = ndb.StringProperty(required = True)
     first_name = ndb.StringProperty(required = True)
     last_name = ndb.StringProperty(required = True)
     email = ndb.StringProperty(required = True)
@@ -9,11 +9,9 @@ class User(ndb.Model):
 class Item(ndb.Model):
     img_url = ndb.StringProperty(required = True)
     description = ndb.StringProperty(required = True)
-    owner = ndb.KeyProperty(User)
+    owner = ndb.KeyProperty(UserProfile)
 
 class Like(ndb.Model):
-    item = ndb.KeyPropery(Item)
-    owner = ndb.KeyProperty(User)
-
-    liker_id = ndb.IntegerProperty(required = True)
-    item_id = ndb.IntegerProperty(required = True)
+    item = ndb.KeyProperty(Item)
+    owner = ndb.KeyProperty(UserProfile)
+    liker = ndb.KeyProperty(UserProfile)
